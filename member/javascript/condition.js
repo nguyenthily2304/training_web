9718a12970aaefcf7aec2b2fef33a46ca17487a0
LyNguyenThi <lynt.pnv@gmail.com>
2016-09-22 17:15:05 +0700

    update search feature, JSON data

--- member/javascript/condition.js
+++ member/javascript/condition.js
@@ -1,51 +1,99 @@
-// var members = [{
-//   "name":"John Jperis",
-//   "Dob" :"1995/04/12",
-//   "Job" :"Web Development"
-//   "phone": 0988481091,
-//   "email": "jons@gmail.com"
-//   "Bio" :"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quisquam ad, tenetur assumenda laudantium, exercitationem? Explicabo harum impedit, aspernatur reiciendis nemo est! Tempora atque, cupiditate ab officia qui! Doloribus, doloremque."
-//   },
-//   {
-//   "name":"Dekih Javes",
-//   "Dob" :"1995/02/16",
-//   "Job" :"Design"
-//   "phone": 00124481091,
-//   "email": "dekihg@gmail.com"
-//   "Bio" :"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quisquam ad, tenetur assumenda laudantium, exercitationem? Explicabo harum impedit, aspernatur reiciendis nemo est! Tempora atque, cupiditate ab officia qui! Doloribus, doloremque."
-//   },
-//   {
-//   "name":"Vekcos Ahis",
-//   "Dob" :"1990/12/25",
-//   "Job" :"PHP Development"
-//   "phone": 01235481091,
-//   "email": "vekcos@gmail.com"
-//   "Bio" :"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quisquam ad, tenetur assumenda laudantium, exercitationem? Explicabo harum impedit, aspernatur reiciendis nemo est! Tempora atque, cupiditate ab officia qui! Doloribus, doloremque."
-//   }];
-//   var obj = JSON.parse(members);
-//   document.getElementById("result".innerHTML= obj.members +<br>;
+var members = [{
+  "id"   : 1,
+  "name" :"John Jperis",
+  "avatar": "images/01.png",
+  "dob"  :"1995/04/12",
+  "job"  :"Web Development",
+  "phone": "0988481091",
+  "email": "jons@gmail.com",
+  "bio"  :"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quisquam ad, tenetur assumenda laudantium, exercitationem? Explicabo harum impedit, aspernatur reiciendis nemo est! Tempora atque, cupiditate ab officia qui! Doloribus, doloremque."
+  },
+  {
+  "id"   : 2,
+  "name" :"Dekih Javes",
+  "avatar": "images/02.png",
+  "dob"  :"1995/02/16",
+  "job"  :"Design",
+  "phone": "00124481091",
+  "email": "dekihg@gmail.com",
+  "bio"  :"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quisquam ad, tenetur assumenda laudantium, exercitationem? Explicabo harum impedit, aspernatur reiciendis nemo est! Tempora atque, cupiditate ab officia qui! Doloribus, doloremque."
+  },
+  {
+  "id"   : 3,
+  "name" :"Vekcos Ahis",
+  "avatar": "images/08.png",
+  "dob"  :"1990/12/25",
+  "job"  :"PHP Development",
+  "phone": "01235481091",
+  "email": "vekcos@gmail.com",
+  "bio"  :"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi quisquam ad, tenetur assumenda laudantium, exercitationem? Explicabo harum impedit, aspernatur reiciendis nemo est! Tempora atque, cupiditate ab officia qui! Doloribus, doloremque."
+  }];
+  // console.log(members[0].id)
+  window.onload = function() {
+    var dataMember ="";
+      for (var i = 0; i < members.length; i++) {
+        dataMember += '<div class="row divKeywords">';
+          dataMember += '<div class="col-md-4">';
+            dataMember += '<img src="' + members[i].avatar +'" alt="" class="img-responsive"/>'; 
+          dataMember += '</div>';  
+          dataMember += '<div class="col-md-8">' ;
+            dataMember += '<h3 class="keywords">' + members[i].name + '</h3>';
+            dataMember += '<p>' + members[i].job + '</p>';
+            dataMember += '<date>'+ members[i].dob +'</date>';
+          dataMember += '</div>';
+        dataMember += '</div>';
+      }
+    document.getElementById("dataMember").innerHTML = dataMember;
+  }
+  function search() { 
+    var key = document.getElementById("key");    
+    var name = members[name];
+    var resultContainer = [];
 
-function search() { 
-  var key = document.getElementById("key").value;    
-  var data = document.getElementsByClassName("divKeywords");
-  var keywords = document.getElementsByClassName("keywords");
-
-  var resultContainer = [];   
-  for (var i = 0; i < data.length; i++) {
-    if(keywords[i].innerHTML.indexOf(key) != -1){
-      resultContainer.push(data[i].innerHTML);
-      data[i].style.display="none";
-    }
-    if(resultContainer.length  <= 0){    
-      data[i].style.display="none";
-    }
+    for (var i = 0; i < members.length; i++) {
+      if((members[i].name).indexOf(key.value) !== -1 ){
+        resultContainer.push(members[i].name.innerHTML);
+        members[i].style.display="none";
+      }
+      if(resultContainer.length  <= 0){    
+        members[i].style.display="none";
+      }
   };
   var getOption = document.getElementById("optionSort");//get id
   var option = getOption.options[getOption.selectedIndex].value;//*by text/get value to sort
   var sortBy = document.getElementsByName("typeSort");
-    resultContainer.sort();
-    resultContainer.reverse();
-  var html = '<div class="row">';
-    html += resultContainer.toString();
-    document.getElementById("result").innerHTML = html;
+  resultContainer.sort();
+  resultContainer.reverse();
+  var html = '<hr>';
+  html += resultContainer.toString();
+  document.getElementById("dataMember").innerHTML = html;
 }
+  // function search() { 
+  //   var key = document.getElementById("key");
+  //   var name = members[name];
+  //   // var regexp = /[a-z]/i; 
+    
+  //   var resultContainer = [];     
+  //   for (var i = 0; i < members.length; i++) {
+  //     if(members[i].name == key.value){
+  //       var match_value = members[i].name.match(regexp);
+  //       resultContainer.push(JSON.stringify(members[i]));
+  //     }
+  //     // if(resultContainer.length  <= 0){    
+  //     //   members[i].style.display="none";
+  //     // }
+  //   };
+  // console.log(resultContainer);
+  // var getOption = document.getElementById("optionSort");//get id
+  // var option = getOption.options[getOption.selectedIndex].value;//*by text/get value to sort
+  // var sortBy = document.getElementsByName("typeSort");
+  // resultContainer.sort();
+  // resultContainer.reverse();
+//   var html = '<hr>';
+//   html += resultContainer.toString();
+//   document.getElementById("dataMember").innerHTML = html;
+// }
+// function redirectToDetailbyId(clicked) { // target// refers to the clicked element
+//   var idDetails = document.getElementById("list").id;
+//   location.href='detail.html'.idDetails;
+// };
--- member/javascript/details.js
+++ member/javascript/details.js
@@ -1,4 +1,7 @@
-function redirectorId{
-	var local = document.getElementById('moveOther');
-	// var localId = document.getElementsByClassName('');
-}
+// function redirectorId{
+// 	var local = document.getElementById('moveOther');
+// 	// var localId = document.getElementsByClassName('');
+// }
+// function redirectorDetails(target) { // Target refers to the clicked element
+//     location.href='details.html';
+// };
