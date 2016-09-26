a6da1317e168541facde6fd0a9de9e9a192a6787
lynguyenthi <lynt.pnv@gmail.com>
2016-09-26 08:54:03 +0700

    Delete details.js

--- member/javascript/details.js
+++
@@ -1,7 +0,0 @@
-// function redirectorId{
-// 	var local = document.getElementById('moveOther');
-// 	// var localId = document.getElementsByClassName('');
-// }
-// function redirectorDetails(target) { // Target refers to the clicked element
-//     location.href='details.html';
-// };
